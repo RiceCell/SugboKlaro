480a4d8a4018cc11fdf3c5104549b4817b98ae4b
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-08-16 11:04:52 +0800

    Working navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,23 @@
 <template>
-    <section class="w-full h-20 bg-slate-800">
+    <section class="absolute z-50 w-full h-20 bg-linear-to-b from-slate-800 to-slate-900 flex items-center px-5 shadow-md gap-12">
+        <RouterLink to="/home" class="h-full flex items-center">
+            <img :src="logo" class="h-[80%]">
+        </RouterLink>
 
+        <RouterLink to="/home" 
+            class="text-xl transition-colors hover:text-cyan-500" 
+            active-class="bg-linear-to-br from-cyan-gr-start to-cyan-gr-end bg-clip-text text-transparent underline underline-offset-5">
+            Home
+        </RouterLink>
+        
+        <RouterLink to="/reports" 
+            class="text-xl transition-colors hover:text-cyan-500" 
+            active-class="bg-linear-to-br from-cyan-gr-start to-cyan-gr-end bg-clip-text text-transparent underline underline-offset-5">
+            Reports
+        </RouterLink>
     </section>    
 </template>
+
+<script setup lang="ts">
+import logo from '../assets/SugboKlaroLogo_dark.png'
+</script>
