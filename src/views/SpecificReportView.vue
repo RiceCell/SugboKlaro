a6384c10bc63eec33b58575f36156b300dfa5bf8
lmrkive <licastanos@up.edu.ph>
2026-08-17 10:38:33 +0800

    breadcrumbns

--- src/views/SpecificReportView.vue
+++ src/views/SpecificReportView.vue
@@ -9,7 +9,10 @@
             :data="selectedReport"
         />
     </div>
-    <h1>Report Details for ID: {{ id }}</h1>
+    <h1 @click="router.back()" style="cursor: pointer;">
+      Report Details for ID: {{ id }} >
+  </h1>
+
 
     <div v-if="loading">Loading report data...</div>
     <div v-else-if="error" class="text-red-500">{{ error }}</div>
